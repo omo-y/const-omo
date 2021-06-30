1f6293d344c223cf0cbd22d9ecdc3c2a37e10a2a
omo-y <46962034+omo-y@users.noreply.github.com>
2021-07-01 04:49:51 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import ColorfulMsg from "./components/ColorfulMsg";
+// import CountButton from "./components/CountButton";
+
+// const App = () => {
+//   return (
+//     <>
+//       <h2>Reactハンズオン！</h2>
+//
+//       <ColorfulMsg color="blue">Reactは難しくないよー</ColorfulMsg>
+//       <ColorfulMsg color="green">Vueも難しくないよー</ColorfulMsg>
+//       <ColorfulMsg color="red">Angularは、、、</ColorfulMsg>
+//       <CountButton />
+//     </>
+//   );
+// };
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+const val3 = "const変数";
+console.log(val3);
+//val3="const変数上書き不可";
+//val3="const変数上書き不可"；
+const val4 = {
+  name: "げん",
+  age: 25
+};
+console.log(val4);
+const val5 = ["dog", "cat"];
+val5[0] = "pic";
+val5.push("bird");
+console.log(val5);
